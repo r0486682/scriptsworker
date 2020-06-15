7b58fb269026101ed69408f6961c3359def279da
r0486682 <jordy.dieltjens@student.kuleuven.com>
2020-06-15 12:36:35 +0000

    queue

---
+++ getNetworkids.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+listPrecontainers="$(docker ps | grep karlherler/pause | awk '{print $1}')"
+
+
+while IFS= read -r line; do
+        netId=("$(docker inspect $line | grep NetworkMode | cut -d '"' -f4)")
+        cgroupParent=("$(docker inspect $line | grep CgroupParent  | cut -d '"' -f4)")
+        sharedLibraryPath=("$(docker inspect $line |  grep Source | grep sharedlibrary | cut -d '"' -f4)")
+
+        echo $netId 
+        echo $cgroupParent 
+        echo $sharedLibraryPath
+done <<< "$listPrecontainers"
+
