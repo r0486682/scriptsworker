f28923ee2d08c38da18bc9cad9d2dd81283c6713
r0486682 <jordy.dieltjens@student.kuleuven.com>
2020-06-22 17:00:50 +0000

    update

--- clearAllConsumer.sh
+++ clearAllConsumer.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 listJavaConsumers="$(docker ps --all | grep java-consumer | awk '{print $1}')"
+count="$(docker ps --all | grep -c java-consumer)"
 
-while IFS= read -r line; do
-    docker stop "$line"
-    docker rm "$line"
-done <<< "$listJavaConsumers"
+if [ "$count" != 0 ]  ;
+        then
+          while IFS= read -r line; do
+             docker stop "$line"
+             docker rm "$line"
+          done <<< "$listJavaConsumers"
+fi
