39832f67ce73227d2606bb6b9d8733ec27c21e81
r0486682 <jordy.dieltjens@student.kuleuven.com>
2020-08-06 18:19:16 +0000

    t'

--- createConsumer.sh
+++ createConsumer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #docker run -d  --memory=0 -v "$3:/app/lib"  --cpu-shares="368"  --cpu-period="100000" --cgroup-parent="$2" --cpu-quota="36000" --ipc="$1" --net="$1" -e "DNS_NAMESPACE=gold" -e "STRESS_SIZE=100" -e "POOL_SIZE=10" r0486682/java-consumer:testinit
-#docker run -d  --memory=0 -v --cpu-shares="368"  --cpu-period="100000" --cgroup-parent="$2" --cpu-quota="36000" --ipc="$1" --net="$1" -e "DNS_NAMESPACE=gold" -e "STRESS_SIZE=100" -e "POOL_SIZE=10" r0486682/java-consumer:split
-docker run -d  --memory=0 -v --cpu-shares="368"  --cpu-period="100000" --cgroup-parent="$2" --cpu-quota="36000" --ipc="$1" --net="$1" -e "DNS_NAMESPACE=gold" -e "STRESS_SIZE=100" -e "POOL_SIZE=10" arguez/java-consumer
+docker run -d  --memory=0 -v --cpu-shares="368"  --cpu-period="100000" --cgroup-parent="$2" --cpu-quota="36000" --ipc="$1" --net="$1" -e "DNS_NAMESPACE=gold" -e "STRESS_SIZE=100" -e "POOL_SIZE=10" r0486682/java-consumer:split
+#docker run -d  --memory=0 -v --cpu-shares="368"  --cpu-period="100000" --cgroup-parent="$2" --cpu-quota="36000" --ipc="$1" --net="$1" -e "DNS_NAMESPACE=gold" -e "STRESS_SIZE=100" -e "POOL_SIZE=10" arguez/java-consumer
